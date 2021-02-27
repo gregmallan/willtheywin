01c8366789b6127807860dcc5e5abb992c3f16bc
Greg Allan <greg.allan@ubc.ca>
2021-02-26 23:25:01 -0800

    Root redirects to new endpoint will-they-win

--- src/api.py
+++ src/api.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
+def root():
+    return redirect(url_for('will_they_win'))
+
+
+@app.route('/will-they-win/', methods=['GET'])
 def will_they_win():
     return {'answer': 'Not likely...'}
