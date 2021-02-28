97de8813cde89b2883334bd1079ca5d0e1daa923
Greg Allan <greg.allan@ubc.ca>
2021-02-28 01:08:36 -0800

    Added first view tests

--- conftest.py
+++ conftest.py
@@ -1,5 +1,12 @@
 import pytest
 
+from src.api import app as flask_app
+
 
 def pytest_report_header(config):
     return ["PROJECT: willtheywin", ]
+
+
+@pytest.fixture
+def app():
+    return flask_app
